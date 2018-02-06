ccb0267ac1ba399f999599dbba8807fb78a33f76
Rami Awar <rami.awar.ra@gmail.com>
2018-02-06 13:11:52 +0200

    Functional quick script in sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 //Create base population
 var population = [];
-var population_size = 100;
-var target = "Cruella DeVille, Cruella DeVille, if she doesnt scare you no evil thing will"
+var population_size = 1000;
+var target = "Cruella DeVille if she doesnt scare you no evil thing will"
+var generation = [];
+var generation_nb = 0;
+
+var nongraded_retain_percent = 0.2;
+var graded_retain_percent = 0.3;
+
+var mutationRate = 0.001;
 
 var mating_pool;
 
@@ -19,11 +26,15 @@ function setup(){
 
 function draw(){
 
+
 	background(255, 255, 255);
 
 	//Calculate fitness of every element in the population
 	for(var i = 0; i < population_size; i++){
 		population[i].evaluateFitness(target);
+		if(population[i].fitness == 1){
+			noLoop();
+		}
 	}
 
 	//Find max fitness for accurate mapping ( prob = relative fitness )
@@ -36,20 +47,56 @@ function draw(){
 	//Natural selection
 	mating_pool = [];	//Empty mating pool
 
+	for (var i = 0; i < population_size; i++) {
+    	  
+    	var fitness = map(population[i].fitness,0,maxFitness,0,1);
+      	var n = floor(fitness * 100);  // Arbitrary multiplier, we can also use monte carlo method
+      	for (var j = 0; j < n; j++) {            
+        	mating_pool.push(population[i]);
+      	}
+    }
+
+    //TODO: SHUFFLE ARRAY
+
+	//Fill new generation
+	generation_nb++;
+
+	population = population.sort(function(a,b){
+		if(a.fitness == b.fitness) return 0;
+		else return a.fitness > b.fitness? -1:1;
+	});
+
+	//console.log(population);
+
+
 	for(var i = 0; i < population_size; i++){
-
-		//Random number
-		var q = Math.random(1);
-		if(map(population[i].fitness, 0, maxFitness, 0, 1) >= q){
-			mating_pool.push(population[i]);
-			
+		
+		//Retain percentage of most fit chromosomes
+		if(i < graded_retain_percent*population_size){
+			generation[i] = population[i];
+		
+		//Retain percentage of randomly picked chromosomes
+		}else if(i < (graded_retain_percent + nongraded_retain_percent)*population_size){
+			generation[i] = population[Math.floor(random(0, population_size))];
+		
+		//Fill rest of population with children
+		}else{
+			var p1 = mating_pool[Math.floor(random(mating_pool.length))];
+			var p2 = mating_pool[Math.floor(random(mating_pool.length))];
+			generation[i] = p1.crossover(p2);
 		}
-		console.log(population[i].fitness)
 
-		// console.log(map(population[i].fitness, 0, maxFitness, 0, 1))
+		//Mutate genes with probability mutationRate
+		generation[i].mutate(mutationRate);
 	}
 
-	//console.log(mating_pool)
-	
+	population = generation;
+
+	console.log(maxFitness);
+
+	textSize(32);
+	clear();
+	text(population[0].phenotype(), 10, 30);
+	text('Generation :' + generation_nb, 10, 80);
 
 }
